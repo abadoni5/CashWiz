5877e0a03fe497d3976a8b7149ccff8873f89763
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2024-07-28 02:56:49 +0530

    add home header component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import HeaderBox from "@/components/HeaderBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Aayush" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Personal finance management made easy"
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
