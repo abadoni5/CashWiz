01eb19ec03a5926e94b1a176c46537b5fd58a0c4
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2024-07-31 02:16:56 +0530

    add sign up form and sidebar navigation links

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,7 +11,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
-        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2 px-4">
           <Image
             src="/icons/logo.svg"
             width={34}
